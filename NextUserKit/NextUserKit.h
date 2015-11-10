12817fe3da2ea3b42ac7c4dfc6641664937a8e2f
Dino <dino@dreamfactory-2.local>
2015-11-10 12:10:05 +0100

    initial commit

---
+++ NextUserKit/NextUserKit.h
@@ -0,0 +1,20 @@
+//
+//  NextUserKit.h
+//  NextUserKit
+//
+//  Created by Dino on 11/6/15.
+//  Copyright © 2015 NextUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NextUserKit.
+FOUNDATION_EXPORT double NextUserKitVersionNumber;
+
+//! Project version string for NextUserKit.
+FOUNDATION_EXPORT const unsigned char NextUserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NextUserKit/PublicHeader.h>
+
+
+#import <NextUserKit/NUTracker.h>
