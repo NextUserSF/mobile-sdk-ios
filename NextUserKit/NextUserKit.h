846e1c11df78c99b3852bc803e2c97535eaab15c
Dino <dino.bartosak@gmail.com>
2015-12-16 10:41:38 +0100

    Umbrella header imports ordering.

--- NextUserKit/NextUserKit.h
+++ NextUserKit/NextUserKit.h
@@ -12,7 +12,7 @@ FOUNDATION_EXPORT double NextUserKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char NextUserKitVersionString[];
 
 #import <NextUserKit/NUTracker.h>
+#import <NextUserKit/NUAction.h>
 #import <NextUserKit/NUPurchase.h>
 #import <NextUserKit/NUPurchaseItem.h>
 #import <NextUserKit/NUPurchaseDetails.h>
-#import <NextUserKit/NUAction.h>
