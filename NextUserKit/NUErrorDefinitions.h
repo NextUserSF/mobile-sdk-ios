a225bba4591c5114039d10b2280948f16721d827
Dino <dino.bartosak@gmail.com>
2016-02-02 16:30:30 +0100

    Documented error definitions

--- NextUserKit/NUErrorDefinitions.h
+++ NextUserKit/NUErrorDefinitions.h
@@ -6,6 +6,12 @@
 //  Copyright © 2016 NextUser. All rights reserved.
 //
 
+/**
+ *  NextUser default error domain.
+ */
 NSString * const NUNextUserErrorDomain;
 
+/**
+ *  NextUser general error code.
+ */
 NSInteger const NUNextUserErrorCodeGeneral;
