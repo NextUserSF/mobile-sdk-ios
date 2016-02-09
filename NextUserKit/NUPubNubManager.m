58f91cf586b5f33e5fa3d70fefaa29c4563072e7
Dino <dino.bartosak@gmail.com>
2016-02-09 13:03:21 +0100

    Branched: pubnub Added PubNub library files and PubNubManager container.

---
+++ NextUserKit/NUPubNubManager.m
@@ -0,0 +1,13 @@
+//
+//  NUPubNubManager.m
+//  NextUserKit
+//
+//  Created by Dino on 2/9/16.
+//  Copyright © 2016 NextUser. All rights reserved.
+//
+
+#import "NUPubNubManager.h"
+
+@implementation NUPubNubManager
+
+@end
