0508341843afff38a2c4544d9d83ef0c599b6135
Dino <dino.bartosak@gmail.com>
2017-01-30 17:37:09 +0100

    delete build folder

--- Example/build/Pods.build/Debug-iphoneos/NextUser.build/DerivedSources/NextUser_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char NextUserVersionString[];
- extern const double NextUserVersionNumber;
-
- const unsigned char NextUserVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:NextUser  PROJECT:Pods-1" "\n";
- const double NextUserVersionNumber __attribute__ ((used)) = (double)1.;
--- Example/build/Pods.build/Debug-iphoneos/Pods-NextUser_Example.build/DerivedSources/Pods_NextUser_Example_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_NextUser_ExampleVersionString[];
- extern const double Pods_NextUser_ExampleVersionNumber;
-
- const unsigned char Pods_NextUser_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_NextUser_Example  PROJECT:Pods-1" "\n";
- const double Pods_NextUser_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
