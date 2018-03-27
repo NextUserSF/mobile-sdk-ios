ec968ac3c6c8e9a1d8808a734926047a8863d3d2
Adrian Lazea <a.lazea@gmail.com>
2018-03-28 01:01:10 +0300

    version

--- NextUser/Classes/NUEvent.m
+++ NextUser/Classes/NUEvent.m
@@ -140,7 +140,7 @@
 {
     NSMutableString *parametersString = [NSMutableString stringWithString:@""];
     
-    // max 10 parameters are allowed
+    // max 11 parameters are allowed
     if (actionParameters.count > 11) {
         actionParameters = [actionParameters subarrayWithRange:NSMakeRange(0, 10)];
     }
